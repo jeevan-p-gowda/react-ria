daf4fcf05f47ee193d3e77514f47f47e04203c7c
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-03-17 12:48:23 +0530

    conditional rendering

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,18 @@
 function Product(props){
     const data = props.pdata
+    const renderStock = () => {
+        if(data.productStock > 0){
+            return (<button onClick={()=> props.btnClick(data.productId)}>Add to Cart</button>);
+        } 
+        return <h5>Out of Stock</h5>;
+    };
 
     return (
         <div>
             <img src={data.productImage} />
             <h4>{data.productName}</h4>
             <h5>Price: {data.productPrice}</h5>
-            <button onClick={()=> props.btnClick(data.productId)}>Add to Cart</button>
+            {renderStock()}
         </div>
     )
 }
