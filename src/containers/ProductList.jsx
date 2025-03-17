0c56f8002599b5f60ef0ee58e63cab1e184cd8d8
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-03-17 13:07:29 +0530

    handling list of items

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,17 +1,31 @@
 import Product from "../components/Product";
 
 function ProductList(){
-    const plist = {
+    const plist = [{
         productId: 1,
         productName: "Laptop",
         productPrice: 50000,
         productImage: "",
         productStock: 10,
-    }
+    },
+    {
+        productId: 2,
+        productName: "Mobile",
+        productPrice: 15000,
+        productImage: "",
+        productStock: 0,
+    },
+    {
+        productId: 3,
+        productName: "Tablet",
+        productPrice: 20000,
+        productImage: "",
+        productStock: 5,
+    }];
     return (
         <div>
-            <Product pdata ={plist}
-            btnClick={(id)=>console.log('add item',id)} />
+            {plist.map((item) => (<Product pdata ={item}
+            btnClick={(id)=>console.log('add item',id)} />))}
         </div>
     );
 }
